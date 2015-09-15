66c5df870a40859b7b7f6600a3d96e1bcae012b5
zsimo <zsimo30@gmail.com>
2015-09-16 00:21:45 +0200

    added web worker

--- ud860-browser-rendering-optimization/lesson04/worker.js
+++ ud860-browser-rendering-optimization/lesson04/worker.js
@@ -1,7 +1,10 @@
 this.onmessage = function (e) {
 
-	var data = e.data;
+	var data = e.data.message;
+	var pippo = e.data.pippo;
 	console.log(data);
+	console.log(pippo);
+
 
 	this.postMessage("message from worker");
 
