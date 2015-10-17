da7f23e288f6dabca37151cf91c6981702e6279b
unknown <Simone.Sacchi@andromeda.nubilaria-eu.local>
2015-10-17 18:31:54 +0200

    async prog first lesson

---
+++ Asynchronous-Programming-in-JavaScript(FrontendMaster-2015)/04IteratorsAndObserver.js
@@ -0,0 +1,9 @@
+console.log("================================");
+console.log("ITERATORS and OBSERVER");
+console.log("");
+
+// Events and Arrays are both COLLECTIONS
+
+// ITERATORS:
+// - producer
+// - consumer
