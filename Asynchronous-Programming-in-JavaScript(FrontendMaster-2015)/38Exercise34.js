43149873ceefe14113d26353218ee65f15042477
Simone Sacchi <simone.sacchi@nubilaria.com>
2015-11-22 23:44:27 +0100

    first custom obervable creation using $.getJSON

--- Asynchronous-Programming-in-JavaScript(FrontendMaster-2015)/38Exercise34.js
+++ Asynchronous-Programming-in-JavaScript(FrontendMaster-2015)/38Exercise34.js
@@ -1,6 +1,20 @@
-var Purdy = require('purdy');
-var rx = require("rx");
-var ajax = require("jquery","ajax");
+// var rx = require("rx");
+// var ajax = require("jquery","ajax");
 
 
-Purdy(ajax);
+var Observable = Rx.Observable;
+var textbox = document.getElementById("textbox");
+
+
+var searchWikiedia = function (term){
+	var url = "http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search="+ encodeURIComponent(term) +"&callback=?";
+	$.getJSON(url, function(data){
+		console.log(data[1]);
+	})
+};
+searchWikiedia("pippo");
+
+var keypresses = Observable.fromEvent(textbox, "keypress");
+keypresses.forEach(function(e){
+	console.log(e);
+});
