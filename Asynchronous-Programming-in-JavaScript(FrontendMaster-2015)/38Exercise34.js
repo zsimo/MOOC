b10f76abebe9c5571066f5bff65c1ea899ab4c2d
Simone Sacchi <simone.sacchi@nubilaria.com>
2015-11-23 23:55:30 +0100

    autocomplete 01

--- Asynchronous-Programming-in-JavaScript(FrontendMaster-2015)/38Exercise34.js
+++ Asynchronous-Programming-in-JavaScript(FrontendMaster-2015)/38Exercise34.js
@@ -4,6 +4,7 @@
 
 var Observable = Rx.Observable;
 var textbox = document.getElementById("textbox");
+// var results = document.getElementById("results");
 
 
 var searchWikiedia = function (term){
@@ -12,9 +13,74 @@ var searchWikiedia = function (term){
 		console.log(data[1]);
 	})
 };
-searchWikiedia("pippo");
+// searchWikiedia("pippo");
+
+
+// create an observable from $.JSON
+var getWikipdiaSearchResults = function(term) {
+		// short form
+		return Observable.create(function forEach(observer) {
+			var cancelled = false;
+			var url = "http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search="+ encodeURIComponent(term) +"&callback=?";
+				$.getJSON(url, function(data){
+					if (!cancelled) {
+						observer.onNext(data[1]);
+						// only one call
+						observer.onCompleted();
+					}
+				});
+
+				return function dispose() {
+					cancelled = true;
+				};
+			});
+	// long form
+	// return {
+	// 	forEach : function(observer) {
+	// 		var cancelled = false;
+	// 		var url = "http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search="+ encodeURIComponent(term) +"&callback=?";
+	// 		$.getJSON(url, function(data){
+	// 			if (!cancelled) {
+	// 				observer.onNext(data[1]);
+	// 				// only one call
+	// 				observer.onCompleted();
+	// 			} 
+	// 			return {
+	// 				dispose : function() {
+	// 					cancelled = true;
+	// 				}
+	// 			}
+	// 		})
+	// 	}
+	// };
+};
+
+// getWikipdiaSearchResults("pippo").forEach(function(results){
+// 	console.log(results);
+// });
 
 var keypresses = Observable.fromEvent(textbox, "keypress");
-keypresses.forEach(function(e){
-	console.log(e);
-});
+var searchResultsSet = keypresses
+						.throttle(20)
+						.map(function(e) {
+							return textbox.value;
+						})
+						.distinctUntilChanged()
+						.filter(function(search){
+							return search.trim().length > 0;
+						})
+						.map(function(search) {
+							// return [String.fromCharCode(e.charCode)];
+							return getWikipdiaSearchResults(search);
+						})
+						.switchLatest();
+
+searchResultsSet.forEach(function(resultSet){
+							// console.log(result);
+							// results.value = JSON.stringify(resultSet);
+							results.value = resultSet;
+						},
+						function(error){
+							console.log(error);
+						});
+
