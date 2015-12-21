63de4cc2537f663e93b27de477ebebb92024bfbd
Simone Sacchi <simone.sacchi@nubilaria.com>
2015-12-21 23:20:59 +0100

    - added JavaScript the Good Parts folder - new operator

---
+++ JavaScript-the-Good-Parts(FrontendMaster-2015)/javascriptFundametals-new-operator.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Simone.Sacchi on 21/12/2015.
+ */
+// the new operator do this:
+//function new (func, args) {
+//    var that = Object.create(func.prototype);
+//    var result = func.apply(that, args);
+//
+//    return (typeof result === 'object' && result) || that;
+//}
+
+console.log("Ok");
