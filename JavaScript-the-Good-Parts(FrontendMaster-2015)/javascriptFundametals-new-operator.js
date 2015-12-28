d6803728d635766fc0bb38473efa03a4b65eba06
Simone Sacchi <simone.sacchi@nubilaria.com>
2015-12-29 00:09:42 +0100

    ---

--- JavaScript-the-Good-Parts(FrontendMaster-2015)/javascriptFundametals-new-operator.js
+++ JavaScript-the-Good-Parts(FrontendMaster-2015)/javascriptFundametals-new-operator.js
@@ -2,11 +2,11 @@
  * Created by Simone.Sacchi on 21/12/2015.
  */
 // the new operator do this:
-//function new (func, args) {
-//    var that = Object.create(func.prototype);
-//    var result = func.apply(that, args);
-//
-//    return (typeof result === 'object' && result) || that;
-//}
+function new (func, args) {
+   var that = Object.create(func.prototype);
+   var result = func.apply(that, args);
+
+   return (typeof result === 'object' && result) || that;
+}
 
 console.log("Ok");
---
+++ JavaScript-the-Good-Parts(FrontendMaster-2015)/javascriptFundametals-prototype.js
@@ -0,0 +1,8 @@
+var obj1 = {
+    "test" : "pippo"
+};
+
+var obj2 = Object.create(obj1);
+
+console.log(obj1);
+console.log(obj2);
