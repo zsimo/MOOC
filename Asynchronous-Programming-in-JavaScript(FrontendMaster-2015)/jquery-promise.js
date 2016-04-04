49a72cb1c9a9f46eab010058c583958acf388435
zsimo <zsimo30@gmail.com>
2016-04-04 23:55:03 +0200

    added: jquery-promise example

--- Asynchronous-Programming-in-JavaScript(FrontendMaster-2015)/jquery-promise.js
+++ Asynchronous-Programming-in-JavaScript(FrontendMaster-2015)/jquery-promise.js
@@ -4,6 +4,14 @@
 
 // https://jsbin.com/vohiwexido/edit?js,console
 
+// https://api.jquery.com/jquery.when/
+// In the multiple-Deferreds case where one of the Deferreds is rejected,
+// jQuery.when() immediately fires the failCallbacks for its master Deferred.
+// Note that some of the Deferreds may still be unresolved at that point.
+// The arguments passed to the failCallbacks match the signature of the failCallback for the Deferred that was rejected.
+// If you need to perform additional processing for this case, such as canceling any unfinished Ajax requests,
+// you can keep references to the underlying jqXHR objects in a closure and inspect/cancel them in the failCallback.
+
 var operations = (function () {
     var actions = [];
 
@@ -54,7 +62,7 @@ var callbacks = {
     },
     fail : function (obj) {
         console.log("fail");
-        operations.stop();
+        //operations.stop();
     }
 };
 
