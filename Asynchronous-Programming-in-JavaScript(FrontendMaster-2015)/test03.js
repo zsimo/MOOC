539abedf0a058f8ea3abd22716c697e74abc8189
Simone Sacchi <simone.sacchi@nubilaria.com>
2016-05-06 18:26:06 +0200

    - testing with pausable

--- Asynchronous-Programming-in-JavaScript(FrontendMaster-2015)/test03.js
+++ Asynchronous-Programming-in-JavaScript(FrontendMaster-2015)/test03.js
@@ -1,48 +1,63 @@
 /**
  * Created by simonesacchi on 28/04/16.
  */
-var checkVariationObserver = Rx.Observable.startAsync(
-    function () {
-        return $.getJSON("test-variations.json");
-    })
+var checkVariationObserver = window.Rx.Observable.fromPromise($.getJSON("test-variations.json"))
     .map(function (item) {
-        item.name = "variations_added";
+        console.log(item);
+        //item.name = "variations_added";
         return item;
     })
-    .delay(1000)
-    ;
+    .delay(100)
+;
+
+
+
+//var a =
+
+
+
 
 var updateObserver = Rx.Observable.startAsync(
     function () {
         return $.getJSON("test-update.json");
     })
     .map(function (item) {
-        item.name = "update_added";
+        console.log(item);
+        //item.name = "update_added";
         return item;
     })
-    .delay(500)
+    .delay(1000)
+    //.take(1)
     ;
 
 
 var observable = Rx.Observable
                     .concat(checkVariationObserver, updateObserver)
                     .map(function (item) {
-                        if (item.test === "variations") {
-                            item.name = "variations_added";
-                        } else if (item.test === "update") {
-                            item.name = "update_added";
-                        } else {
-                            item.name = "???";
-                        }
+                        //if (item.test === "variations") {
+                        //    item.name = "variations_added";
+                        //} else if (item.test === "update") {
+                        //    item.name = "update_added";
+                        //} else {
+                        //    item.name = "???";
+                        //}
 
                         return item;
                     })
+    .pausable()
+    //.takeUntil(abortObservbl);
     ;
 
 
 observable.subscribe(
     function (itemEmitted) {
-        console.log(itemEmitted);
+        console.log("onNext: " , itemEmitted);
+        if (itemEmitted.variations === "ok") {
+            //observable.pause();
+            //modal();
+
+        }
+
     }
     , function (err) {
         console.log(err);
@@ -52,15 +67,14 @@ observable.subscribe(
         console.log("completed");
     }
 );
-
+observable.resume();
 
 
 var clicks = Rx.Observable.fromEvent(document.getElementById("button"), "click");
-var timerObserver = Rx.Observable.interval(1000)
-                    .takeUntil(clicks);
-timerObserver.subscribe(
+var obseerver = clicks.subscribe(
     function (item) {
-        console.log(item);
+        observable.resume();
+        obseerver.dispose();
     }
     ,function (err) {
         console.log(err);
@@ -69,3 +83,16 @@ timerObserver.subscribe(
         console.log("onCompleted by clicking");
     }
 );
+//var timerObserver = Rx.Observable.interval(1000)
+//                    .takeUntil(clicks);
+//timerObserver.subscribe(
+//    function (item) {
+//        console.log(item);
+//    }
+//    ,function (err) {
+//        console.log(err);
+//    }
+//    ,function () {
+//        console.log("onCompleted by clicking");
+//    }
+//);
