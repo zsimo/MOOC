232225abdbe47a59c9a19b631e032e8b3dffd6a4
zsimo <zsimo30@gmail.com>
2017-05-31 23:50:24 +0200

    add webpack dev server setup

--- Save-time-avoiding-common-mistakes-using-RxJS/webapack.config.babel.js
+++
@@ -1,6 +0,0 @@
-module.exports = env => ({
-    output: {
-        filename: env.cache ? "[chunkhash].js" : "bundle.js", 
-        chunkFilename: env.cache ? "[chunkhash].js" : "[id].chunk.js
-    }
-});
