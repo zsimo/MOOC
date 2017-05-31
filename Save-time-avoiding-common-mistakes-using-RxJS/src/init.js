5d7d1483523925ff0c95dde7f6c5a9784ec6da33
zsimo <zsimo30@gmail.com>
2017-06-01 00:07:12 +0200

    add first rxjs example

--- Save-time-avoiding-common-mistakes-using-RxJS/src/init.js
+++ Save-time-avoiding-common-mistakes-using-RxJS/src/init.js
@@ -1,1 +1,13 @@
-console.log("heress");
+"use strict";
+
+var Rx = require("rxjs");
+
+var click$ = new Rx.Subject();
+
+document.addEventListener("click", function (ev) {
+    click$.next(ev);
+});
+
+click$.subscribe(function (ev) {
+    console.log(ev);
+});
