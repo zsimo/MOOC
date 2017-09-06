5317be8e1dc1294137c4693420044e60bae97b35
zsimo <zsimo30@gmail.com>
2017-09-06 23:54:17 +0200

    add webpack dev derver base setup

---
+++ Deep-JavaScript-Foundations/src/init.js
@@ -0,0 +1,11 @@
+/**
+ * Created by simonesacchi on 06/09/2017.
+ */
+
+
+document.write("<h1>Hello!</h1>");
+
+
+var shadow = require("./shadow");
+
+shadow();
---
+++ Deep-JavaScript-Foundations/src/shadow.js
@@ -0,0 +1,11 @@
+
+
+var pippo = "uno";
+
+function run () {
+    var pippo = "due";
+    console.log(pippo);
+    console.log(window.pippo);
+}
+
+module.exports = run;
