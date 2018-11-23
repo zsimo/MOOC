44169f4e2cbcaafafde6c2e7e5c5d1c4f72d322c
zsimo <zsimo30@gmail.com>
2018-11-23 01:05:44 +0100

    add new example

--- JavaScript-The-Hard-Parts-of-Object-Oriented-JavaScript/new.js
+++ JavaScript-The-Hard-Parts-of-Object-Oriented-JavaScript/new.js
@@ -3,15 +3,28 @@
 
 function userCreator (name, score) {
 
+    // 1.
     // new crea un nuovo oggetto
     // var newUser = Object.create(userFunctionStore);
+
+    // this is automatically bounded to an empty object
+    //var this = {};
+
+    // 2. new bounds this.__proto__ to userCreator.prototype
 
     this.name = name;
     this.score = score;
 
+
+    // 3.
     // new ritorna il nuovo oggetto creato
-    // return newUser;
+    // return this;
 }
+
+userCreator.prototype.increment = function () {
+    this.score ++;
+};
 
 
 var user1 = new userCreator("simo", 4);
+user1.increment();
